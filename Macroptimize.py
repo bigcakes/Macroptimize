1ea68c5eaee87c7dcd5b2595dbafd9a50e5abdb6
bigcakes <jjjsgmw@hotmail.com>
2014-12-07 10:29:22 -0500

    Making changes to the code based on FichteFoll's suggestions.  Much cleaner now.

--- Macroptimize.py
+++ Macroptimize.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, dis, inspect, os, glob
+import sublime, sublime_plugin, os, collections
 
 class MacroptimizeCommand(sublime_plugin.TextCommand):
 	items = []
@@ -6,19 +6,11 @@ class MacroptimizeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.items = [] #refresh the items in the list each new run
 
-		packagesRoot = sublime.packages_path()
-		baseDirectory = os.path.basename(packagesRoot)
+		#Thanks to FichteFoll for letting me know about this much easier to use API
+		macroFiles = sublime.find_resources("*.sublime-macro")
 
-		for root, dirs, files in os.walk(packagesRoot):
-			relativeDirectory = os.path.relpath(root, packagesRoot)
-
-			for file in files:
-				if file.endswith(".sublime-macro"):
-					relativeAndBase = os.path.join(baseDirectory, relativeDirectory)
-					relativeFilePath = os.path.join(relativeAndBase, file)
-
-					#not sure if Sublime requires this format on all OS's, need to test
-					self.items.append(macro(file, relativeFilePath.replace("\\", "/")))
+		for file in macroFiles:
+			self.items.append(Macro(os.path.basename(file), file))
 
 		#Ask user which macro to run
 		sublime.active_window().show_quick_panel(self.getUserChoices(), self.handleMacro)
@@ -28,10 +20,7 @@ class MacroptimizeCommand(sublime_plugin.TextCommand):
 			self.view.run_command("run_macro_file", {"file":self.items[choice].fullPath})
 
 	def getUserChoices(self):
-		return [str(index) + " " + macro.prettyText for index, macro in enumerate(self.items)]
+		return ["%d %s" % (index, macro.prettyText) for index, macro in enumerate(self.items)]
 
 #Simple class for holding macro information
-class macro:
-	def __init__(self, prettyText, fullPath):
-		self.fullPath = fullPath
-		self.prettyText = prettyText
+Macro = collections.namedtuple("Macro", ["prettyText", "fullPath"])
--- README.md
+++ README.md
@@ -3,7 +3,23 @@ Macroptimize
 
 ##### A plugin for Sublime Text 3 to make working with macros less clunky.
 
-##### Allows for quick selection of any macro to run in Sublime packages. Just ctrl+alt+z and select which macro to run.
+##### Allows for quick selection of any macro to run in Sublime packages.
+
+## Installation
+
+Install this plugin with [Package Control](https://sublime.wbond.net/).
+
+## Usage
+
+Just ctrl+alt+z and select which macro to run.
+
+## TODO
+
+-Repeat until end of file setting (maybe even per macro)
+-Replace current buffer macro with last run macro
+-Key binding for running last selected macro
+
+## Miscellaneous
 
 ##### Any questions or issues with the plugin, please let me know: johnwiegert@outlook.com
 
