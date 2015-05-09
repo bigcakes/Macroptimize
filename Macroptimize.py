4dce006fe6505c3ed2d1623a7010c4471e45532a
bigcakes <jjjsgmw@hotmail.com>
2015-05-09 00:38:37 -0400

    Added messaging for install and updates to the package. Added a new feature for running the last ran macro. Updated readme.

--- Macroptimize.py
+++ Macroptimize.py
@@ -2,22 +2,27 @@ import sublime, sublime_plugin, os, collections
 
 class MacroptimizeCommand(sublime_plugin.TextCommand):
 	items = []
+	lastMacro = None
 
-	def run(self, edit):
-		self.items = [] #refresh the items in the list each new run
+	def run(self, edit, runLast):
+		if (runLast and self.lastMacro is not None):
+			self.view.run_command("run_macro_file", {"file": self.lastMacro})
+		else:
+			self.items = [] #refresh the items in the list each new run
 
-		#Thanks to FichteFoll for letting me know about this much easier to use API
-		macroFiles = sublime.find_resources("*.sublime-macro")
+			#Thanks to FichteFoll for letting me know about this much easier to use API
+			macroFiles = sublime.find_resources("*.sublime-macro")
 
-		for file in macroFiles:
-			self.items.append(Macro(os.path.basename(file), file))
+			for file in macroFiles:
+				self.items.append(Macro(os.path.basename(file), file))
 
-		#Ask user which macro to run
-		sublime.active_window().show_quick_panel(self.getUserChoices(), self.handleMacro)
+			#Ask user which macro to run
+			sublime.active_window().show_quick_panel(self.getUserChoices(), self.handleMacro)
 	
 	def handleMacro(self, choice):
 		if (choice > -1):
-			self.view.run_command("run_macro_file", {"file":self.items[choice].fullPath})
+			self.lastMacro = self.items[choice].fullPath
+			self.view.run_command("run_macro_file", {"file": self.lastMacro})
 
 	def getUserChoices(self):
 		return ["%d %s" % (index, macro.prettyText) for index, macro in enumerate(self.items)]
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Macroptimize
 
 ## Installation
 
-Install this plugin with [Package Control](https://sublime.wbond.net/).
+Install this plugin with [Package Control](https://packagecontrol.io/packages/Macroptimize).
 
 ## Usage
 
@@ -15,12 +15,10 @@ Just ctrl+alt+z and select which macro to run.
 
 ## TODO
 
--Repeat until end of file setting (maybe even per macro)
--Replace current buffer macro with last run macro
--Key binding for running last selected macro
+- Repeat until end of file setting (maybe even per macro)
+- Replace current buffer macro with last run macro
+- ~~Key binding for running last selected macro~~
 
 ## Miscellaneous
 
 ##### Any questions or issues with the plugin, please let me know: johnwiegert@outlook.com
-
-Please go easy on me if any of this is poor python style, this was my first time using the language for a project.
